05f2720262a13c4a27f2bead1705610642498f81
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-26 22:53:46 +0700

    feature(ViewModels): Add view model of MainWindow.

---
+++ StudyTracker/ViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿namespace StudyTracker.ViewModel.ViewModels
+{
+    using StudyTracker.Stores;
+    using StudyTracker.ViewModel.Commands;
+    using StudyTracker.ViewModel.MVVM;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Data context of main window.
+    /// </summary>
+    class MainViewModel: ViewModelBase
+    {
+        /// <summary>
+        /// Navigation store.
+        /// </summary>
+        private readonly NavigationStore _navigationStore;
+
+        /// <summary>
+        /// Instance of current view model of main field of window.
+        /// </summary>
+        public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
+
+        /// <summary>
+        /// Data context of side panel.
+        /// </summary>
+        public SidePanelViewModel SidePanelViewModel;
+
+        /// <summary>
+        /// Command that change main field of window.
+        /// </summary>
+        public ICommand NavigeteTo { get; }
+
+        /// <summary>
+        /// Initialise instance of <see cref="MainViewModel"/>.
+        /// </summary>
+        /// <param name="navigationStore">Navigation store.</param>
+        public MainViewModel(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+            _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+
+            NavigeteTo = new NavigateMainFieldCommand(_navigationStore);
+            SidePanelViewModel = new SidePanelViewModel(NavigeteTo);
+        }
+
+        /// <summary>
+        /// Action to change main field of window.
+        /// </summary>
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+    }
+}
