253f066a0487da6c9f0685baac827f8c0e975f89
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-26 22:48:04 +0700

    feature(Stores): Add navigation store.

---
+++ StudyTracker/Stores/NavigationStore.cs
@@ -0,0 +1,37 @@
+﻿namespace StudyTracker.Stores
+{
+    using StudyTracker.ViewModel.MVVM;
+
+    /// <summary>
+    /// Store of navigation.
+    /// </summary>
+    public class NavigationStore
+    {
+        /// <summary>
+        /// Current viewModel of main part of the window.
+        /// </summary>
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        /// <summary>
+        /// Action of CurrentViewModel changed.
+        /// </summary>
+        public event Action CurrentViewModelChanged;
+
+        /// <summary>
+        /// Event of CurrentViewModel changed.
+        /// </summary>
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
