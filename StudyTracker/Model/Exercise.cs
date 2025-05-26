8d44942cb3de7ff1efad977c7f7bb83fdf9ab64b
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-26 22:47:43 +0700

    feature(Model): Add exercise tree-structure.

---
+++ StudyTracker/Model/Exercise.cs
@@ -0,0 +1,48 @@
+﻿namespace StudyTracker.Model
+{
+    /// <summary>
+    /// Exercise that user want to do.
+    /// </summary>
+    class Exercise
+    {
+        /// <summary>
+        /// Name of exercise.
+        /// </summary>
+        private string _name = string.Empty;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        /// <summary>
+        /// Description of the exercise.
+        /// </summary>
+        private string _description = string.Empty;
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        /// <summary>
+        /// Main exercise. Also parent of that node in tree.
+        /// </summary>
+        private Exercise? _parent = null;
+        public Exercise Parent
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        /// <summary>
+        /// Subexercises. Children of that node in tree.
+        /// </summary>
+        private List<Exercise>? _children = null;
+        public List<Exercise> Children
+        {
+            get { return _children; }
+            set { _children = value; }
+        }
+    }
+}
