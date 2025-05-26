aef9ef15bae10a6dd388b9f04204e57c10d213e7
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-26 22:49:37 +0700

    feature(Enums): Add enum of types that main window can be changed.

---
+++ StudyTracker/ViewModel/Enums/MainFieldType.cs
@@ -0,0 +1,23 @@
+﻿namespace StudyTracker.ViewModel.Enums
+{
+    /// <summary>
+    /// Types of main field of the window.
+    /// </summary>
+    public enum MainFieldType
+    {
+        /// <summary>
+        /// Calendar.
+        /// </summary>
+        Calendar,
+
+        /// <summary>
+        /// List of all exercises.
+        /// </summary>
+        Backlog,
+
+        /// <summary>
+        /// Gant diagram.
+        /// </summary>
+        GantDiagram,
+    }
+}
