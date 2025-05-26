ea30a3f316bc76b05d76ba9104405c30f3b5fdd3
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-26 22:49:03 +0700

    feature(Commands): Add command to change main field of window.

---
+++ StudyTracker/ViewModel/Commands/CommandBase.cs
@@ -0,0 +1,33 @@
+﻿namespace StudyTracker.ViewModel.Commands
+{
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Base class to create commands.
+    /// </summary>
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        /// <summary>
+        /// Check ability to execute command.
+        /// </summary>
+        /// <param name="parameter">Command parameter.</param>
+        /// <returns>True - command can be executed.</returns>
+        public virtual bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Command action.
+        /// </summary>
+        /// <param name="parameter">Command parameter.</param>
+        public abstract void Execute(object? parameter);
+
+        protected void OnCanExecutedChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ StudyTracker/ViewModel/Commands/NavigateMainFieldCommand.cs
@@ -0,0 +1,55 @@
+﻿namespace StudyTracker.ViewModel.Commands
+{
+    using StudyTracker.Stores;
+    using StudyTracker.ViewModel.Enums;
+    using StudyTracker.ViewModel.ViewModels;
+
+    /// <summary>
+    /// Command that change main field of window.
+    /// </summary>
+    public class NavigateMainFieldCommand : CommandBase
+    {
+        /// <summary>
+        /// Store of navigation.
+        /// </summary>
+        private readonly NavigationStore _navigationStore;
+
+        /// <summary>
+        /// Initiate the instance of <see cref="NavigateMainFieldCommand"/>.
+        /// </summary>
+        /// <param name="navigationStore">Store of navigation.</param>
+        public NavigateMainFieldCommand(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+        }
+
+        /// <summary>
+        /// Change the main field of window.
+        /// </summary>
+        /// <param name="parameter">Type of main field. Have to be <see cref="MainFieldType"/></param>
+        public override void Execute(object? parameter)
+        {
+            if(parameter.GetType() == typeof(MainFieldType))
+            {
+                switch (parameter)
+                {
+                    case MainFieldType.Calendar:
+                        {
+                            _navigationStore.CurrentViewModel = new CalendarViewModel();
+                            break;
+                        }
+                    case MainFieldType.GantDiagram:
+                        {
+                            _navigationStore.CurrentViewModel = new GantDiagrammViewModel();
+                            break;
+                        }
+                    case MainFieldType.Backlog:
+                        {
+                            _navigationStore.CurrentViewModel = new BacklogViewModel();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
