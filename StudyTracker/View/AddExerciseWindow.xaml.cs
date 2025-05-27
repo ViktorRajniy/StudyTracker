0ee18e0eac3635e904ed8ec348e30410ba0cf067
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-27 15:22:39 +0700

    feature(View): Add view to add exercise.

---
+++ StudyTracker/View/AddExerciseWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace StudyTracker.View
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Логика взаимодействия для AddExercise.xaml
+    /// </summary>
+    public partial class AddExerciseWindow : Window
+    {
+        public AddExerciseWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
